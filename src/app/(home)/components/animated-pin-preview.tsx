c845210367c46bdecbb696b7f41148105dd264af
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-04-09 09:36:48 +0200

    fix(BeeMonitor)

--- src/app/(home)/components/animated-pin-preview.tsx
+++ src/app/(home)/components/animated-pin-preview.tsx
@@ -14,7 +14,7 @@ export function AnimatedPinDemo() {
                 <h2 className="text-center text-2xl font-bold text-black dark:text-white py-8">
                     BeeMonitor
                 </h2>
-                <PinContainer title="/Github/BuzzVerse" href="https://github.com/BuzzVerse">
+                <PinContainer title="BuzzVerse" href="https://github.com/BuzzVerse">
                     <div
                         className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                         <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
