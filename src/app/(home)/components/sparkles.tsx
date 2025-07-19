50f93bec33fae971dc3c3a603339bf2eb4fb3432
Dawid Feruś <ferero546@gmail.com>
2025-07-19 10:57:40 +0200

    fixed sparkles component

--- src/app/(home)/components/sparkles.tsx
+++ src/app/(home)/components/sparkles.tsx
@@ -4,30 +4,30 @@ import {SparklesCore} from "@/components/ui/sparkles-core";
 
 export function Sparkles() {
     return (
-        <div className="w-full h-auto md:h-screen min-h-[80vh] md:min-h-screen bg-neutral-950 flex flex-col items-center justify-center overflow-hidden rounded-md py-8 md:py-0">
-            <h1 className="md:text-7xl text-5xl lg:text-9xl font-bold text-center text-white relative z-20">
+        <div className="w-full h-screen bg-neutral-950 flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="md:text-7xl text-5xl lg:text-9xl font-bold text-center text-white relative z-20 px-4">
                 BuzzVerse
             </h1>
-            <div className="w-screen h-40 relative">
+            <div className="w-full h-32 md:h-40 relative">
                 {/* Gradients */}
                 <div
-                    className="absolute  left-1/3 top-0 bg-gradient-to-r from-transparent via-Buzzprimary to-transparent h-[2px] w-1/2 "/>
+                    className="absolute left-1/2 transform -translate-x-1/2 top-0 bg-gradient-to-r from-transparent via-Buzzprimary to-transparent h-[2px] w-1/2 md:w-1/3 "/>
                 <div
-                    className="absolute left-1/4 top-0 bg-gradient-to-r from-transparent via-Buzzprimary to-transparent h-[5px] w-1/2 blur-sm"/>
+                    className="absolute left-1/2 transform -translate-x-1/2 top-0 bg-gradient-to-r from-transparent via-Buzzprimary to-transparent h-[5px] w-2/3 md:w-1/2 blur-sm"/>
 
                 {/* Core component */}
                 <SparklesCore
                     background="transparent"
-                    minSize={0.4}
-                    maxSize={1}
-                    particleDensity={1200}
+                    minSize={0.3}
+                    maxSize={0.8}
+                    particleDensity={600}
                     className="w-full h-full"
                     particleColor="#FFFFFF"
                 />
 
                 {/* Radial Gradient to prevent sharp edges */}
                 <div
-                    className="absolute inset-0 w-full h-full bg-neutral-950 [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+                    className="absolute inset-0 w-full h-full bg-neutral-950 [mask-image:radial-gradient(200px_120px_at_top,transparent_20%,white)] md:[mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
             </div>
         </div>
     );
